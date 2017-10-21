0fcff73fb416f988468ab40c3bf89871e8764c5a
Rajath <simplytalk95@gmail.com>
2017-10-21 17:04:08 +0530

    Configured with mysql successfully

--- src/primeExchange/models.py
+++ src/primeExchange/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class user_data(models.Model):
+	def __str__(self):
+		return self.full_name
+
+	full_name=models.CharField(max_length=20)
+
+
+
