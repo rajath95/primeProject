6d687efd9209d950b90b95828d77afcf100dcf2f
Rajath <simplytalk95@gmail.com>
2017-10-21 15:29:37 +0530

    Second commit

--- src/prime/settings.py
+++ src/prime/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'prime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+             'read_default_file':'/etc/mysql/my.cnf'
+         },
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
