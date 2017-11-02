6288c0d7293969d5d6948ee11f2bb225929e4d93
Rajath <simplytalk95@gmail.com>
2017-11-02 14:38:10 +0530

    Added password input funtionality

--- src/primeExchange/forms.py
+++ src/primeExchange/forms.py
@@ -9,7 +9,7 @@ class LoginForm(forms.Form):
 	username = forms.CharField(label="Username", max_length=30, 
 							   widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
 	password = forms.CharField(label="Password", max_length=30, 
-							   widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
+							   widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
 	def clean(self,*args,**kwargs):
 		username=self.cleaned_data.get("username")
 		password=self.cleaned_data.get("password")
